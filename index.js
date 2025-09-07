4ba82ebb565398ca9a9183720431cba49e294bc1
komed3 <pk97@outlook.de>
2025-09-07 15:35:59 +0200

    Create lib/generator.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const { loadConfig } = require( './lib/config' );
+const { generateConfig } = require( './lib/generator' );
 const { log } = require( './lib/logger' );
 
 async function main () {}
